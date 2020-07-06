6c2093ff8d4421c48333ac4a2b8b47dd53b9c1a1
Daniel Tian <dtian.95@gmail.com>
2020-07-06 15:12:55 +0800

    modified env

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,12 +5,12 @@ switch (process.env.NODE_ENV) {
   case "development":
     console.log("Environment is 'development'");
     configDotenv({
-      path: resolve(__dirname, "../dev.env"),
+      path: resolve(__dirname, "../../dev.env"),
     });
     break;
   case "test":
     configDotenv({
-      path: resolve(__dirname, "../test.env"),
+      path: resolve(__dirname, "../../test.env"),
     });
     break;
   // Add 'staging' and 'production' cases here as well!
